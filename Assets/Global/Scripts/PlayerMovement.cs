314c0dfb9c574be9ca96adc70995a96cf38f14e4
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-10-29 19:22:11 +0100

    adding custom grafity to the script
    todo for next time:
    
    there should be 2 vectors. One for the player, and one for the camera. and the requested movement should be derived from the camera and applied to the player.
    Aka, right should be the rigt of the camera, which is just force applied to the body. The model will follow the direction.
    And the camera will try to follow the model.
    then you have full circle again

--- Assets/Global/Scripts/PlayerMovement.cs
+++ Assets/Global/Scripts/PlayerMovement.cs
@@ -4,46 +4,74 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")] 
     public float movementSpeed = 1f;
-    public float rotationSpeed = 1f;
+    public float rotationSpeed = 100f;
     public Transform orientation;
+    
+    [Header("Custom Gravity")]
+    public float gravityForce = 9.81f;
+    public Vector3 gravityDirection = Vector3.down;
+    public float GravityFloorCheckDistance  = 2f;
     
     private float horizontalInput;
     private float verticalInput;
     Vector3 moveDirection;
-    Rigidbody rb;
+    
+    private Rigidbody rb;
+    private Collider col;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        rb.useGravity = false; 
         rb.freezeRotation = true;
     }
     
     void Update()
     {
-        MyInput();
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
     
     void FixedUpdate()
     {
         MovePlayer();
-    }
-    
-    private void MyInput()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        ApplyCustomGravity();
     }
     
     private void MovePlayer()
     {
-        moveDirection = orientation.forward * verticalInput;
-        moveDirection.y = 0;
-
-        rb.AddForce(moveDirection.normalized * movementSpeed, ForceMode.VelocityChange);
+        // Calculate movement direction based on the input and orientation
+        this.moveDirection = this.orientation.forward * this.verticalInput + 
+                             this.orientation.right * this.horizontalInput;
+        this.moveDirection = this.moveDirection.normalized * this.movementSpeed;
         
-        if (horizontalInput != 0)
-        {
-            orientation.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
-        }
+        // Apply force to move the player
+        this.rb.AddForce(this.moveDirection, ForceMode.Acceleration);
+    }
+    
+    private void ApplyCustomGravity()
+    {
+        var bounds = this.col.bounds;
+        var position = bounds.center - new Vector3(0, bounds.extents.y, 0);
+        
+        // Cast a ray downwards from the player position
+        // If it hits something, than the inverse of that normal is the gravity direction to make the player stick to that surface
+        // if it doesnt hit anything, the direction returns back to straight down
+        if (Physics.Raycast(position, this.gravityDirection , out var hit,
+                            this.GravityFloorCheckDistance ))
+            this.gravityDirection = -hit.normal;
+        else
+            this.gravityDirection = Vector3.down;
+        
+        // Calculate the target rotation so that "up" aligns with the inverse of gravityDirection
+        // that way the player actually sticks to the surface
+        var targetUp = -this.gravityDirection.normalized;
+        var targetRotation = Quaternion.FromToRotation(this.transform.up, targetUp) * this.transform.rotation;
+        this.rb.rotation = Quaternion.Slerp(this.rb.rotation, targetRotation, 
+                                            this.rotationSpeed * Time.fixedDeltaTime);
+        
+        var gravity = this.gravityDirection.normalized * this.gravityForce;
+        this.rb.AddForce(gravity, ForceMode.Acceleration);
     }
 }
