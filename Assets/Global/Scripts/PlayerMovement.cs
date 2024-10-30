2eaa14affaf06767a2e9f48f4773f50f7febc561
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-10-30 22:58:55 +0100

    progress

--- Assets/Global/Scripts/PlayerMovement.cs
+++ Assets/Global/Scripts/PlayerMovement.cs
@@ -1,77 +1,105 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool showInGizmos = false;
+    
     [Header("Movement")] 
-    public float movementSpeed = 1f;
-    public float rotationSpeed = 100f;
-    public Transform orientation;
+    public float movementSpeed = 100f;
+    public float modelRotationSpeed = 10f; // relevant for how snappy the gravity rotation is. And also for rotating to left and right
+    // This rotation speed influences to much, we probalby really need a oriontation object. 
+    // This in its basic form its just a transform. However, when you rotate, press D, you can move the velocity already 90 degress
+    // and then lerp this vector to that velocity vector, and the camera attaches to that then ofcourse.
+    // (you can also make it when you already have some speed, that the A and D button have less power basiccally)
+    // other things:
+    // - jump
+    // - grafity that makes sence
+    // - based on speed, a max slope 
+    
     
     [Header("Custom Gravity")]
-    public float gravityForce = 9.81f;
-    public Vector3 gravityDirection = Vector3.down;
-    public float GravityFloorCheckDistance  = 2f;
+    public float gravityForce = 29.81f;
+    public float gravityFloorCheckDistance  = 1.5f;
+    private Vector3 _gravityDirection = Vector3.down;
     
-    private float horizontalInput;
-    private float verticalInput;
-    Vector3 moveDirection;
+    private float _horizontalInput;
+    private float _verticalInput;
+    private Vector3 _moveDirection;
     
-    private Rigidbody rb;
-    private Collider col;
+    private Rigidbody _rb;
+    private Transform _playerTransform;
     
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        col = GetComponent<Collider>();
-        rb.useGravity = false; 
-        rb.freezeRotation = true;
+        this._rb = this.GetComponent<Rigidbody>();
+        this._playerTransform = this.GetComponent<Transform>();
+        this._rb.useGravity = false; 
+        this._rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        this._horizontalInput = Input.GetAxis("Horizontal");
+        this._verticalInput = Input.GetAxis("Vertical");
     }
     
-    void Update()
+    private void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-    }
-    
-    void FixedUpdate()
-    {
-        MovePlayer();
-        ApplyCustomGravity();
+        this.MovePlayer();
+        this.ApplyCustomGravity();
+        this.RotatePlayer();
     }
     
     private void MovePlayer()
     {
         // Calculate movement direction based on the input and orientation
-        this.moveDirection = this.orientation.forward * this.verticalInput + 
-                             this.orientation.right * this.horizontalInput;
-        this.moveDirection = this.moveDirection.normalized * this.movementSpeed;
-        
-        // Apply force to move the player
-        this.rb.AddForce(this.moveDirection, ForceMode.Acceleration);
+        this._moveDirection = this._playerTransform.forward * this._verticalInput + 
+                             this._playerTransform.right * this._horizontalInput;
+        this._moveDirection = this._moveDirection.normalized * this.movementSpeed;
+  
+        this._rb.AddForce(this._moveDirection, ForceMode.Acceleration);
     }
     
     private void ApplyCustomGravity()
     {
-        var bounds = this.col.bounds;
-        var position = bounds.center - new Vector3(0, bounds.extents.y, 0);
-        
-        // Cast a ray downwards from the player position
-        // If it hits something, than the inverse of that normal is the gravity direction to make the player stick to that surface
+        // Here we cast a ray downwards from the player position
+        // If it hits something, than the inverse of the normal of the hit is the gravity direction to make the player stick to that surface
         // if it doesnt hit anything, the direction returns back to straight down
-        if (Physics.Raycast(position, this.gravityDirection , out var hit,
-                            this.GravityFloorCheckDistance ))
-            this.gravityDirection = -hit.normal;
+        var position = this._playerTransform.position;
+        if (Physics.Raycast(position, this._gravityDirection , out var hit,
+                            this.gravityFloorCheckDistance ))
+            this._gravityDirection = -hit.normal;
         else
-            this.gravityDirection = Vector3.down;
+            this._gravityDirection = Vector3.down;
         
-        // Calculate the target rotation so that "up" aligns with the inverse of gravityDirection
-        // that way the player actually sticks to the surface
-        var targetUp = -this.gravityDirection.normalized;
-        var targetRotation = Quaternion.FromToRotation(this.transform.up, targetUp) * this.transform.rotation;
-        this.rb.rotation = Quaternion.Slerp(this.rb.rotation, targetRotation, 
-                                            this.rotationSpeed * Time.fixedDeltaTime);
+        if (this.showInGizmos)
+            Debug.DrawRay(position,this._gravityDirection, Color.red,  0f, false);
+
+        // Now that we are also correctly rotated, we can apply the "new" gravity to the player.
+        var gravity = this._gravityDirection.normalized * this.gravityForce;
+        this._rb.AddForce(gravity, ForceMode.Acceleration);
+    }
+
+    
+    private void RotatePlayer()
+    {  
+        // We want the players forward direction, aka, its velocity, however, that can also go down if you are falling.
+        // So we project it on a plane to cancel out the y-ax velocity
+        var projectedDirection = Vector3.ProjectOnPlane(this._rb.linearVelocity, this._gravityDirection);
+        if (this.showInGizmos)
+            Debug.DrawRay(this._playerTransform.position,projectedDirection, Color.cyan,  0f, false);
         
-        var gravity = this.gravityDirection.normalized * this.gravityForce;
-        this.rb.AddForce(gravity, ForceMode.Acceleration);
+        // If there's enough movement to determine a forward direction, we rotate it according to the velocity direction, and its gravity
+        if (projectedDirection.sqrMagnitude > 0.01f)
+        {
+            var targetRotation = Quaternion.LookRotation(projectedDirection, -this._gravityDirection);
+            this._rb.rotation = Quaternion.Slerp(this._rb.rotation, targetRotation, this.modelRotationSpeed * Time.fixedDeltaTime);
+        }
+        else // However, if this magnitude is to low, we rotate the player only by its gravity, ignoring the direction it is facing
+        {
+            var gravityAlignedRotation = Quaternion.FromToRotation(this.transform.up, -this._gravityDirection) * this.transform.rotation;
+            this._rb.rotation = Quaternion.Slerp(this._rb.rotation, gravityAlignedRotation, this.modelRotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
