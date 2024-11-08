2ade103211fed46b66234c161243886c37a714ee
Pim van Milt <pim.milt@gmail.com>
2024-11-08 13:58:10 +0100

    healthbar import

--- Assets/Global/Scripts/Player.cs
+++ Assets/Global/Scripts/Player.cs
@@ -1,17 +1,23 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    
+    // health
+    public Image healthbar;
+    [Range(10, 1000)]
+    public int maxHealth = 100;
+    private float health;
+
     public float jumpforce = 2f;
     public float speed = 5f;
     public int doubleJumps = 1;
     public float glideDrag = 2f;
-    
+
     [HideInInspector]
     public BaseState currentState;
-    [HideInInspector] 
+    [HideInInspector]
     public int currentJumps = 0;
     [HideInInspector]
     public float horizontalInput;
@@ -24,9 +30,10 @@ public class Player : MonoBehaviour
 
     [Header("Debugging")]
     public string currentStateName;
-    
+
     void Start()
     {
+        health = maxHealth;
         playerRb = GetComponent<Rigidbody>();
         SetState(new IdleState(this));
     }
@@ -38,7 +45,7 @@ public class Player : MonoBehaviour
         currentState.Update();
         currentStateName = currentState.GetType().Name;
     }
-    
+
     void FixedUpdate() => currentState.FixedUpdate();
 
     private void OnCollisionEnter(Collision collision)
@@ -50,12 +57,26 @@ public class Player : MonoBehaviour
     {
         isGrounded = !collision.gameObject.CompareTag("Ground");
     }
-    
+
     public void SetState(BaseState newState)
     {
-        if ( currentState!= null) 
+        if (currentState != null)
             currentState.Exit();
         currentState = newState;
         currentState.Enter();
+    }
+
+    public void OnHit(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    public void OnDeath()
+    {
+        Debug.Log("PLAYER DIED", this);
     }
 }
