1f148d4854952bb7ace6c07844f55c71781cd02b
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-11-08 10:27:03 +0100

    Update JumpingState.cs

--- Assets/Global/Scripts/States/JumpingState.cs
+++ Assets/Global/Scripts/States/JumpingState.cs
@@ -7,10 +7,10 @@ public class JumpingState : BaseState
     {
     }
 
-    [System.Obsolete]
+ 
     public override void Update()
     {
-        player.playerRb.velocity = new Vector3(player.playerRb.velocity.x, 0, player.playerRb.velocity.z);
+        player.playerRb.linearVelocity = new Vector3(player.playerRb.linearVelocity.x, 0, player.playerRb.linearVelocity.z);
         player.playerRb.AddForce(Vector3.up * player.jumpforce, ForceMode.Impulse);
         player.SetState(new FallingState(player));
     }
