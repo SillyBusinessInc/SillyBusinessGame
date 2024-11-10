94d4358e0384a0f591e0e0bdda5be8542035c517
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2024-11-10 18:55:58 +0100

    Infinite attack

--- Assets/Global/Scripts/Attack/Attack.cs
+++ Assets/Global/Scripts/Attack/Attack.cs
@@ -1,15 +1,38 @@
+using System.Data;
 using UnityEngine;
+using UnityEngine.UIElements;
 public class Attack : MonoBehaviour
 {
     public Transform target;
-    public float rotationSpeed = 10f;
     public bool rotateLeft;
 
     public float rotation;
 
+    public float speed;
+
     void FixedUpdate()
     {
-        transform.RotateAround(target.position, Vector3.up, rotation);
-        rotation += 1;
+        for (int i = 0; i < speed; i++) //Forloop to make the rotation speed more smooth
+        {
+            if(rotation >= 180.0f)
+            {
+                rotateLeft = true;
+            }
+            if(rotation <= 0.0f)
+            {
+                rotateLeft = false;
+            }
+            if (rotateLeft)
+            {
+                transform.RotateAround(target.position, Vector3.up, 1);
+                rotation -= 1;
+            }
+            else
+            {
+                transform.RotateAround(target.position, Vector3.up, -1);
+                rotation += 1;
+            }
+            Debug.Log(rotation);
+        }
     }
 }
