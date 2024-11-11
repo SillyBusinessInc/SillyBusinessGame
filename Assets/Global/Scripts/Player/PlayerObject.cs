ace64e8128f9ce67521cc8fbf0af387a260f33c2
siwaipang <sisi_pang@hotmail.nl>
2024-11-11 11:42:31 +0100

    jump on top of object jump again

--- Assets/Global/Scripts/Player/PlayerObject.cs
+++ Assets/Global/Scripts/Player/PlayerObject.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerObject : MonoBehaviour
 {
     public Player player;
-    private void OnCollisionEnter(Collision collision) => player.OnCollisionEnter(collision);
-
-    private void OnCollisionExit(Collision collision) => player.OnCollisionExit(collision);
+    void OnCollisionEnter(Collision collision)  {
+        float groundCheckAngle = 60f;
+        if (!collision.gameObject.CompareTag("Ground")) { // if the player touches something that is not the ground
+            foreach (ContactPoint contact in collision.contacts) {
+                float angle = Vector3.Angle(contact.normal, Vector3.up);
+                
+                if (angle <= groundCheckAngle) { // if players jumps on top of it
+                    player.rb.linearVelocity = new Vector3(player.rb.linearVelocity.x, 0, player.rb.linearVelocity.z);
+                    player.rb.AddForce(Vector3.up * player.jumpForce +  Vector3.forward * 2.0f, ForceMode.Impulse); // made the player jump again after touching
+                    break;
+                }
+            }
+        }
+        player.OnCollisionEnter(collision);
+    }
+    
+    void OnCollisionExit(Collision collision) => player.OnCollisionExit(collision);
 }
