a97947b0e7b5f737793a9ea11771b70d544fecf0
siwaipang <sisi_pang@hotmail.nl>
2024-11-12 14:36:11 +0100

    apply feedback

---
+++ Assets/Dev/Si Wai/JumpingMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class JumpingMovement : MonoBehaviour
+{
+    public float bounceForceUp = 5.0f;
+    public float bounceForceForward = 0.1f;
+    public float topAngleThreshold = 105.0f;
+    
+    void OnCollisionEnter(Collision collision)  {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb) {
+            foreach (ContactPoint contact in collision.contacts) {
+                float angle = Vector3.Angle(contact.normal, Vector3.up);
+                
+                // if an object gets on top of the object this script is attached to
+                if (angle >= topAngleThreshold) {
+                    rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+                    rb.AddForce(Vector3.up * bounceForceUp +  rb.transform.forward * bounceForceForward, ForceMode.Impulse); // bounce effect
+                    break;
+                }
+            }
+        }
+    }
+}
--- Assets/Global/Scripts/Player/PlayerObject.cs
+++ Assets/Global/Scripts/Player/PlayerObject.cs
@@ -3,21 +3,7 @@ using UnityEngine;
 public class PlayerObject : MonoBehaviour
 {
     public Player player;
-    void OnCollisionEnter(Collision collision)  {
-        float groundCheckAngle = 60f;
-        if (!collision.gameObject.CompareTag("Ground")) { // if the player touches something that is not the ground
-            foreach (ContactPoint contact in collision.contacts) {
-                float angle = Vector3.Angle(contact.normal, Vector3.up);
-                
-                if (angle <= groundCheckAngle) { // if players jumps on top of it
-                    player.rb.linearVelocity = new Vector3(player.rb.linearVelocity.x, 0, player.rb.linearVelocity.z);
-                    player.rb.AddForce(Vector3.up * player.jumpForce +  Vector3.forward * 2.0f, ForceMode.Impulse); // made the player jump again after touching
-                    break;
-                }
-            }
-        }
-        player.OnCollisionEnter(collision);
-    }
+    void OnCollisionEnter(Collision collision) => player.OnCollisionEnter(collision);
     
     void OnCollisionExit(Collision collision) => player.OnCollisionExit(collision);
 }
