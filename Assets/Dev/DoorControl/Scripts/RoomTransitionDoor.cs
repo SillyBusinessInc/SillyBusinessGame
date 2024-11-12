85411a84db1d8c4f318ced2d8459367b8662df5e
Pim van Milt <pim.milt@gmail.com>
2024-11-12 15:45:41 +0100

    colorchanging door based on lock-status

--- Assets/Dev/DoorControl/Scripts/RoomTransitionDoor.cs
+++ Assets/Dev/DoorControl/Scripts/RoomTransitionDoor.cs
@@ -1,22 +1,50 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class RoomTransitionDoor : MonoBehaviour
 {
+    [Header("Materials")]
+    [SerializeField] private Material lockedMaterial;
+    [SerializeField] private Material unlockedMaterial;
+    [Header("References")]
     [SerializeField] private SceneAsset connectingRoom;
     [SerializeField] private Animator animator;
+    [SerializeField] private MeshRenderer doorMesh;
+
     private bool isLocked = true;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // disable this door when there is no connected room
         this.gameObject.SetActive(connectingRoom ? true : false);
+        toggleLock(true);
     }
 
     void toggleLock(bool v)
     {
         isLocked = v;
+        if (isLocked)
+        {
+            OnLock();
+        }
+        else
+        {
+            OnUnlock();
+        }
+    }
+
+    void OnLock()
+    {
+        doorMesh.SetMaterials(new List<Material> { lockedMaterial });
+    }
+
+    void OnUnlock()
+    {
+        doorMesh.SetMaterials(new List<Material> { unlockedMaterial });
     }
 
     void OpenDoor()
