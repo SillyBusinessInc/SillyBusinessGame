56fab0e2ad4b97a57029997b16155043cb735c54
fabiothomas <95278302+fabiothomas@users.noreply.github.com>
2024-11-13 03:27:13 +0100

    Added base functionality for the room generation and demo scene

--- Assets/Dev/Fabio/RoomGen/RoomGenerator.cs
+++ Assets/Dev/Fabio/RoomGen/RoomGenerator.cs
@@ -1,6 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomGenerator
+public class RoomGenerator : MonoBehaviour
 {
-    
+    public GameObject roomPrefab;
+    private Table table;
+
+    void Start() {
+        table = new();
+        Generate();
+    }
+
+    public void Generate() {
+        foreach(Transform child in transform) {
+            Destroy(child.gameObject);
+        }
+
+        table.Generate();
+        DrawRoomsDebug();
+    }
+
+    public void DrawRoomsDebug() {
+        foreach (Row row in table.table) {
+            GameObject room = Instantiate(roomPrefab, RowPosition(row), Quaternion.identity);
+            room.transform.SetParent(transform, true);
+            room.name = table.RowReference(row);
+            int i = 0;
+            foreach (int branch in row.branches) {
+                // Debug.DrawLine(RowPosition(row), RowPosition(table.GetRow(branch)), Color.black, 100, true);
+                room.GetComponent<LineRenderer>().positionCount = i + 2;
+                room.GetComponent<LineRenderer>().SetPosition(i, RowPosition(row));
+                room.GetComponent<LineRenderer>().SetPosition(i+1, RowPosition(table.GetRow(branch)));
+                i+=2;
+            }
+        }
+    }
+
+    public void ChangeSeed(string value) {
+        if (value == "") value = "-1";
+        table.seed = int.Parse(value);
+    }
+    public void ChangeMaxObjectPerDepth(string value) {
+        if (value == "") value = "0";
+        table.maxObjectPerDepth = int.Parse(value);
+    }
+    public void ChangeMaxBranchCount(string value) {
+        if (value == "") value = "0";
+        table.maxBranchCount = int.Parse(value);
+    }
+    public void ChangeTargetDepth(string value) {
+        if (value == "") value = "0";
+        table.targetDepth = int.Parse(value);
+    }
+
+
+    public Vector3 RowPosition(Row row) => new(2 * row.depth, 1, -2 * table.GetIndexInDepth(row));
 }
--- Assets/Dev/Fabio/RoomGen/Table.cs
+++ Assets/Dev/Fabio/RoomGen/Table.cs
@@ -1,17 +1,91 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+using SRandom = System.Random;
 
 public class Table
 {
-    public List<int> idColumn;
-    public List<int> depthColumn;
-    public List<List<int>> branchesColumn;
-    
-    public struct Row { 
-        public int id; 
-        public int depth; 
-        public List<int> branches; 
+    // Settings
+    public int maxObjectPerDepth = 3;
+    public int maxBranchCount = 3;
+    public int targetDepth = 7;
+    public int seed = -1;
+    private SRandom random;
+
+    // Content
+    public List<Row> table;
+
+    // Constructor
+    public Table() => Generate();
+
+    // Data
+    public Row GetRow(int id) => table.Where(x => x.id == id).FirstOrDefault();
+    public Row GetFirstUnpopulatedRow() => table.Where(x => x.branches.Count == 0).FirstOrDefault(); 
+    public int GetNextId() => table?.Count ?? 0;
+    public int GetIndexInDepth(Row row) => GetRowsAtDepth(row.depth).OrderBy(x => x.id).ToList().FindIndex(x => x.id == row.id);
+    public List<int> GetDepthColumn() => table.Select(x => x.depth).ToList();
+    public List<Row> GetRowsAtDepth(int depth) => table.Where(x => x.depth == depth).ToList();
+
+    // Reference
+    public string RowReference(Row row) => $"Row: {row.id}, {row.depth}, [{string.Join(';', row.branches)}]";
+
+    // Generation Logic
+    public void Generate() {
+        if (seed < 0) random = new(Guid.NewGuid().GetHashCode());
+        else random = new(seed);
+
+        table = new() {new(0, 0)};
+
+        Populate(GetFirstUnpopulatedRow());
+    }
+    private void Populate(Row row) {
+        // base cases
+        if (row.depth == targetDepth) return;
+
+        // population
+        int branchesToApply = random.Next(1, maxBranchCount+1);
+        List<Row> existingBranches = GetRowsAtDepth(row.depth+1);
+        int roomForNewBranches = maxObjectPerDepth - existingBranches.Count();
+        int roomForOldBranches = existingBranches.Count();
+
+        int newBranchesToApply = random.Next(0, Math.Min(roomForNewBranches+1, branchesToApply+1));
+        int oldBranchesToApply = existingBranches.Count() != 0 ? Math.Min(roomForOldBranches, branchesToApply - newBranchesToApply) : 0;
+
+        // Debug.Log($"row: {row.id} | total: {branchesToApply} | new: {newBranchesToApply} | old: {oldBranchesToApply} | room: {roomForNewBranches}");
+
+        for (int i = 0; i < newBranchesToApply; i++ ) {
+            row.branches.Add(GetIdFromNewRow(row.depth+1));
+        }
+        for (int i = 0; i < oldBranchesToApply; i++ ) {
+            row.branches.Add(GetIdFromOldRow(existingBranches));
+        }
+
+        // recursion
+        Populate(GetFirstUnpopulatedRow());
     }
 
+    private int GetIdFromNewRow(int depth) {
+        Row newRow = new(GetNextId(), depth);
+        table.Add(newRow);
+        return newRow.id;
+    }
+    private int GetIdFromOldRow(List<Row> existingBranches) {
+        Row newRow = existingBranches[random.Next(0, existingBranches.Count)];
+        existingBranches.Remove(newRow);
+        return newRow.id;
+    }
+}
 
+public struct Row {
+    public int id;
+    public int depth;
+    public List<int> branches;
+
+    public Row(int id_, int depth_) {
+        id = id_;
+        depth = depth_;
+        branches = new List<int>();
+    }
 }
