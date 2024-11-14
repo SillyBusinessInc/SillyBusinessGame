15a69f0972675ee0cbbcb68b98ae7c78d9a29fd2
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2024-11-14 16:44:05 +0100

    interfal added

--- Assets/Global/Scripts/EnemySpawnArea.cs
+++ Assets/Global/Scripts/EnemySpawnArea.cs
@@ -8,37 +8,20 @@ public class EnemySpawnArea : MonoBehaviour
     [Tooltip("Prefab of the enemy to spawn")]
     [SerializeField] public GameObject enemyPrefab;
 
-    [Tooltip("Spawn interval in seconds")]
-    [Range(0, 30)]
-    [SerializeField] public float spawnInterval = 1f;
-
-    [Tooltip("Maximum number of enemies that can be active at one time")]
-    [SerializeField] public int maxSpawnedEnemies = 5;
+    [SerializeField] public int maxSpawnedEnemies;
 
     [Header("Spawn Area Settings")]
-    [Tooltip("Reference to a Transform object representing the spawn area")]
     [SerializeField] public Transform spawnArea;
 
     private List<GameObject> activeEnemies = new List<GameObject>();
-    private float nextSpawnTime;
 
-    private bool startSpawning = false;
     public bool center = false;
 
     public bool waveDone = false;
 
-    private void Start()
-    {
-        nextSpawnTime = Time.time + spawnInterval;
-    }
-
     private void Update()
     {
-        if (Time.time >= nextSpawnTime && activeEnemies.Count < maxSpawnedEnemies &&startSpawning)
-        {
-            SpawnEnemy();
-            nextSpawnTime = Time.time + spawnInterval;
-        }else if(activeEnemies.Count == maxSpawnedEnemies && activeEnemies.TrueForAll(enemy => enemy == null) &&waveDone)
+        if(activeEnemies.Count == maxSpawnedEnemies && activeEnemies.TrueForAll(enemy => enemy == null) &&waveDone)
         {
             waveDone = false;
             GlobalReference.AttemptInvoke(Events.WAVE_DONE);
@@ -46,40 +29,6 @@ public class EnemySpawnArea : MonoBehaviour
         }
     }
 
-    //function for how many died vs the max spawned enemies like (1/5) it needs to be string
-    [ContextMenu("Get Enemy Count")]
-    public string GetEnemyCount()
-    {
-        int deadEnemies = 0;
-        foreach (GameObject enemy in activeEnemies)
-        {
-            if (enemy == null)
-            {
-                deadEnemies++;
-            }
-        }
-        Debug.Log(deadEnemies + "/" + activeEnemies.Count);
-        return deadEnemies + "/" + activeEnemies.Count;
-    }
-
-    //listen to event
-    private void Awake()
-    {
-        GlobalReference.SubscribeTo(Events.SPAWN_WAVE, startWaveInitials);
-    }
-
-    //stop listening to event
-    private void OnDestroy()
-    {
-        GlobalReference.UnsubscribeTo(Events.SPAWN_WAVE, startWaveInitials);
-    }
-
-    public void startWaveInitials()
-    {
-        activeEnemies.Clear();
-        waveDone = true;
-        startSpawning = true;
-    }    
 
     public void SpawnEnemy()
     {
@@ -109,7 +58,6 @@ public class EnemySpawnArea : MonoBehaviour
         float zPos = Random.Range(-(areaSize.z / 2), (areaSize.z / 2));
         
         // You can adjust this if you want to spawn on a specific surface (e.g., ground level)
-        // Debug.Log(new Vector3(areaCenter.x + xPos, areaCenter.y, areaCenter.z + zPos));
         return new Vector3(areaCenter.x + xPos, areaCenter.y, areaCenter.z + zPos);
     }
 }
--- Assets/Global/Scripts/EnemyWaveManager.cs
+++ Assets/Global/Scripts/EnemyWaveManager.cs
@@ -7,10 +7,8 @@ using Random = UnityEngine.Random;
 public class EnemyWaveManager : MonoBehaviour
 {
     public List<Wave> waves;
-    // public List<GameObject> enemyPrefabs;
     public List<Transform> spawnAreaTransforms;
     public GameObject spawnArea;
-    // public int enemiesPerWave = 5;
     private int totalEnemies;
     private int deadEnemies;
 
@@ -41,7 +39,6 @@ public class EnemyWaveManager : MonoBehaviour
         GlobalReference.SubscribeTo(Events.WAVE_START, () => StartCoroutine(StartWave()));
         GlobalReference.SubscribeTo(Events.WAVE_DONE, WaveCompleted);
         GlobalReference.SubscribeTo(Events.ENEMY_KILLED, OnEnemyDeath);
-        // GlobalReference.SubscribeTo(Events.ENEMY_SPAWNED, () => totalEnemies++);
     }
 
     private void OnDestroy()
@@ -49,7 +46,6 @@ public class EnemyWaveManager : MonoBehaviour
         GlobalReference.UnsubscribeTo(Events.WAVE_START, () => StartCoroutine(StartWave()));
         GlobalReference.UnsubscribeTo(Events.WAVE_DONE, WaveCompleted);
         GlobalReference.UnsubscribeTo(Events.ENEMY_KILLED, OnEnemyDeath);
-        // GlobalReference.UnsubscribeTo(Events.ENEMY_SPAWNED, () => totalEnemies++);
     }
     public IEnumerator StartWave()
     {
@@ -58,33 +54,26 @@ public class EnemyWaveManager : MonoBehaviour
         
         deadEnemies = 0;
         totalEnemies = 0;
-        //count all the enemies in the wave part
-        //take a random spawn area
+        
         var spawner = spawnArea.GetComponent<EnemySpawnArea>();
         totalEnemies = waves[currentWave].waveParts.Sum(wavePart => wavePart.enemyPrefabs.Sum(enemy => enemy.amount));
 
         foreach (var wavePart in waves[currentWave].waveParts) // Access waveParts within each Wave
         {
-            Debug.Log("Total enemies: " + totalEnemies);
-            Debug.Log("Spawn area: " + wavePart.spawnArea);
             spawner.maxSpawnedEnemies = totalEnemies; // Use 'count' if that's the property name
             if (wavePart.spawnArea >= 0)
             {
-                Debug.Log("ghellooooo");
                 spawner.spawnArea = spawnAreaTransforms[wavePart.spawnArea];
                 foreach (var enemy in wavePart.enemyPrefabs) // Loop through each enemy in wavePart
                 {
-                    Debug.Log("ghiiiiiii");
                     // Access the spawn area component and set the enemy properties
                     spawner.enemyPrefab = enemy.enemyPrefab;
                     spawner.waveDone = true;
                     foreach (var _ in Enumerable.Range(0, enemy.amount))
                     {
                         spawner.SpawnEnemy();
-                        yield return new WaitForSeconds(1);
+                        yield return new WaitForSeconds(waves[currentWave].interfal);
                     }
-                    // spawner.startWaveInitials();
-                    // GlobalReference.AttemptInvoke(Events.SPAWN_WAVE);
                 }
             }
             else
@@ -92,7 +81,6 @@ public class EnemyWaveManager : MonoBehaviour
                 Debug.LogError("No spawn area found");
             }
         }
-        Debug.Log("Wave ended");
        
     }
 
