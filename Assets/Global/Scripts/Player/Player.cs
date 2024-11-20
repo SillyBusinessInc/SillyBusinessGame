c8031a6b542c50629b6bb6a543bc2a7383c8c52c
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2024-11-20 20:28:28 +0100

    Player can double jump after slamming

--- Assets/Global/Scripts/Player/Player.cs
+++ Assets/Global/Scripts/Player/Player.cs
@@ -90,6 +90,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        currentJumps = isGrounded ? 0 : currentJumps;
         RaycastDown();
         currentState.Update();
         RotatePlayerObj();
