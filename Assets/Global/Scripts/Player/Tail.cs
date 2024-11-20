04409ddbb363b2835426ae482c8f512a8af052ad
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2024-11-20 14:44:12 +0100

    Player tail does 1 damage for a hit

--- Assets/Global/Scripts/Player/Tail.cs
+++ Assets/Global/Scripts/Player/Tail.cs
@@ -1,14 +1,31 @@
 using System;
 using UnityEngine;
-
+using System.Collections.Generic;
 public class Tail : MonoBehaviour
 {
     public Player player;
 
+    private List<Collider> colliders;
+    public void Start()
+    {
+        colliders = new List<Collider>();
+    }
+    public void Update()
+    {
+        colliders = player.tailCanDoDamage? colliders : new List<Collider>();
+    }
     public void OnTriggerEnter(Collider Collider)
     {
         if (Collider.gameObject.CompareTag("Enemy"))
         {
+            if(!colliders.Contains(Collider))
+            {
+                colliders.Add(Collider);
+            }
+            else
+            {
+                return;
+            }
             if(player.tailCanDoDamage)
             {
                 float actualDamage = player.tailDoDamage * player.playerStatistic.AttackDamageMultiplier.GetValue();
