2218e193272f5be85bc829c10feedeb6c9186985
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-11-21 12:38:38 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Assets/Global/Scripts/Interactables/EventInteractable.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EventInteractable : Interactable
+{
+    [SerializeField] private Events eventje = Events.PICKUP_COLLECTED;
+    [SerializeField] private Events eventje2 = Events.NONE;
+    [SerializeField] private Events eventje3 = Events.NONE;
+    public override void OnInteract()
+    {
+        GlobalReference.AttemptInvoke(eventje);
+        GlobalReference.AttemptInvoke(eventje2);
+        GlobalReference.AttemptInvoke(eventje3);
+    }
+}
