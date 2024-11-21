1c86c0b1b6334da7265ef9febbab6c4569e10409
Isis <89838626+Isissss@users.noreply.github.com>
2024-11-21 15:24:32 +0100

    fix: fix bug

--- Assets/Global/Scripts/Interactables/Interactor.cs
+++ Assets/Global/Scripts/Interactables/Interactor.cs
@@ -8,8 +8,9 @@ public class PlayerInteraction : MonoBehaviour
     private Player player;
 
     [Header("Raycast Settings [Debug]")]
-    [SerializeField] private float rayDistance = 10f;         // Maximum detection distance 
-    [SerializeField] private float rayOffset = 0.5f;         // offset
+    [SerializeField] private float rayDistance = 10f;         // Maximum detection distance  
+    [SerializeField] private float maxInteractionAngle = 180f; // FOV 
+
 
     private bool isColliding = false;
 
@@ -37,25 +38,8 @@ public class PlayerInteraction : MonoBehaviour
         Vector3 origin = transform.position + Vector3.up * 0.1f + transform.forward * -1.0f;
         Vector3 forward = transform.forward;
 
-        Vector3[] offsets =
-        {
-            Vector3.zero,                        // center
-            transform.right * rayOffset,         // right
-            -transform.right * rayOffset,        // left
-            transform.up * rayOffset,            // up
-            -transform.up * rayOffset            // down
-        };
-
-        bool interactableFound = false;
-
-        foreach (Vector3 offset in offsets)
-        {
-            if (RaycastFindInteractable(origin + offset, forward))
-            {
-                interactableFound = true;
-                return;
-            }
-        }
+        // Check if the player is colliding with an interactable
+        bool interactableFound = RaycastFindInteractable(origin, forward);
 
         // If no interactable was found, clear the current interactable
         if (!interactableFound && currentInteractable != null && !isColliding)
@@ -66,43 +50,27 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-
-        var interactable = collision.collider.GetComponent<Interactable>();
-        if (interactable != null)
-        {
-
-            Debug.Log("Collision detected", gameObject);
-            isColliding = true;
-            currentInteractable = interactable;
-            currentInteractable.ShowPrompt(true);
-        }
-        else
-        {
-            isColliding = false;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        isColliding = false;
-        var interactable = collision.collider.GetComponent<Interactable>();
-        if (interactable != null && interactable == currentInteractable)
-        {
-            currentInteractable.ShowPrompt(false);
-            currentInteractable = null;
-        }
-    }
-
     private bool RaycastFindInteractable(Vector3 origin, Vector3 direction)
     {
         Ray ray = new Ray(origin, direction);
         RaycastHit hit;
 
-        // Debug visualization
-        Debug.DrawRay(origin, direction * rayDistance, Color.green, 0.1f);
+        // First check for overlap sphere, to see if the start of the ray is already colliding with an interactable
+        Collider[] colliders = Physics.OverlapSphere(origin, 0.1f, ~ignoreLayers);
+        foreach (var collider in colliders)
+        {
+            var interactable = collider.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                SetInteractable(interactable);
+                return true;
+            }
+        }
 
+        // Debug visualization
+        // Debug.DrawRay(origin, direction * rayDistance, Color.green, 0.1f);
+
+        // Then check for raycast hit
         if (Physics.Raycast(ray, out hit, rayDistance, ~ignoreLayers))
         {
             var interactable = hit.collider.GetComponent<Interactable>();
@@ -113,17 +81,22 @@ public class PlayerInteraction : MonoBehaviour
 
                 if (interactable.IsWithinInteractionRange(rayHitDistance))
                 {
-                    if (currentInteractable != interactable)
-                    {
-                        if (currentInteractable != null) currentInteractable.ShowPrompt(false);
-                        currentInteractable = interactable;
-                        currentInteractable.ShowPrompt(true);
-                    }
-                    return true; // Interactable found
+                    SetInteractable(interactable);
+                    return true;
 
                 }
             }
         }
-        return false; // No valid interactable detected
+        return false;
+    }
+
+    private void SetInteractable(Interactable interactable)
+    {
+        if (currentInteractable != interactable)
+        {
+            if (currentInteractable != null) currentInteractable.ShowPrompt(false);
+            currentInteractable = interactable;
+            currentInteractable.ShowPrompt(true);
+        }
     }
 }
