c36b503dd385952f6d95696894d5a495c58cbc2d
Pim van Milt <pim.milt@gmail.com>
2024-11-25 09:34:36 +0100

    moving folders , initial melee enemy push

---
+++ Assets/Dev/Pim/NEWMelee/Scripts/EnemyBase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace EnemiesNS
+{
+    public abstract class EnemyBase : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+        void OnDeath()
+        {
+
+        }
+
+    }
+}
