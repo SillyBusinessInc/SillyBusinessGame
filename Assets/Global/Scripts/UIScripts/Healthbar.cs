3167c45d559f20996e4b3dff78fd8991bd5bdeca
siwaipang <sisi_pang@hotmail.nl>
2024-11-25 13:58:21 +0100

    apply feedback

--- Assets/Global/Scripts/UIScripts/Healthbar.cs
+++ Assets/Global/Scripts/UIScripts/Healthbar.cs
@@ -7,6 +7,7 @@ public class Healthbar : MonoBehaviour
     private RectTransform HealthbarUnderlayTransform;
     private RectTransform HealthbarOverlayTransform;
     private float originalWidth;
+    private Player player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -14,30 +15,29 @@ public class Healthbar : MonoBehaviour
         HealthbarOverlayTransform = transform.GetChild(1) as RectTransform;
 
         originalWidth = HealthbarUnderlayTransform.sizeDelta.x;
+        player = GlobalReference.GetReference<PlayerReference>().Player;
 
         UpdateMaxHealth();
+        HealthbarOverlayTransform.sizeDelta = new Vector2(
+            originalWidth * (player.playerStatistic.MaxHealth.GetValue() / 2), 
+            HealthbarOverlayTransform.sizeDelta.y
+        );
     }
 
-    public void UpdateHealthBar(float current)
+    public void UpdateHealthBar()
     {
+        float currentHealth = GlobalReference.GetReference<PlayerReference>().Player.playerStatistic.Health;
         HealthbarOverlayTransform.sizeDelta = new Vector2(
-            originalWidth * (current / 2), 
+            originalWidth * (currentHealth / 2), 
             HealthbarOverlayTransform.sizeDelta.y
         );
     }
 
     public void UpdateMaxHealth() {
-        Player player = GlobalReference.GetReference<PlayerReference>().Player;
-
         // doing maxHealth / 2 because 1hp is a half heart
         HealthbarUnderlayTransform.sizeDelta = new Vector2(
             originalWidth * (player.playerStatistic.MaxHealth.GetValue() / 2), 
             HealthbarUnderlayTransform.sizeDelta.y
-        );
-
-        HealthbarOverlayTransform.sizeDelta = new Vector2(
-            originalWidth * (player.playerStatistic.MaxHealth.GetValue() / 2), 
-            HealthbarOverlayTransform.sizeDelta.y
         );
     }
 }
