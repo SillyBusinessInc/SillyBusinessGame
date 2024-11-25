6b104aa535c4ae8ac963114a03fe70309130e2d1
siwaipang <sisi_pang@hotmail.nl>
2024-11-25 13:41:11 +0100

    Update Healthbar.cs

--- Assets/Global/Scripts/UIScripts/Healthbar.cs
+++ Assets/Global/Scripts/UIScripts/Healthbar.cs
@@ -4,25 +4,25 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    private Transform HealthbarUnderlayTransform;
-    private Transform HealthbarOverlayTransform;
+    private RectTransform HealthbarUnderlayTransform;
+    private RectTransform HealthbarOverlayTransform;
     private float originalWidth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        HealthbarUnderlayTransform = transform.GetChild(0);
-        HealthbarOverlayTransform = transform.GetChild(1);
+        HealthbarUnderlayTransform = transform.GetChild(0) as RectTransform;
+        HealthbarOverlayTransform = transform.GetChild(1) as RectTransform;
 
-        originalWidth = (HealthbarUnderlayTransform as RectTransform).sizeDelta.x;
+        originalWidth = HealthbarUnderlayTransform.sizeDelta.x;
 
         UpdateMaxHealth();
     }
 
     public void UpdateHealthBar(float current)
     {
-        (HealthbarOverlayTransform as RectTransform).sizeDelta = new Vector2(
+        HealthbarOverlayTransform.sizeDelta = new Vector2(
             originalWidth * (current / 2), 
-            (HealthbarOverlayTransform as RectTransform).sizeDelta.y
+            HealthbarOverlayTransform.sizeDelta.y
         );
     }
 
@@ -30,14 +30,14 @@ public class Healthbar : MonoBehaviour
         Player player = GlobalReference.GetReference<PlayerReference>().Player;
 
         // doing maxHealth / 2 because 1hp is a half heart
-        (HealthbarUnderlayTransform as RectTransform).sizeDelta = new Vector2(
+        HealthbarUnderlayTransform.sizeDelta = new Vector2(
             originalWidth * (player.playerStatistic.MaxHealth.GetValue() / 2), 
-            (HealthbarUnderlayTransform as RectTransform).sizeDelta.y
+            HealthbarUnderlayTransform.sizeDelta.y
         );
 
-        (HealthbarOverlayTransform as RectTransform).sizeDelta = new Vector2(
+        HealthbarOverlayTransform.sizeDelta = new Vector2(
             originalWidth * (player.playerStatistic.MaxHealth.GetValue() / 2), 
-            (HealthbarOverlayTransform as RectTransform).sizeDelta.y
+            HealthbarOverlayTransform.sizeDelta.y
         );
     }
 }
