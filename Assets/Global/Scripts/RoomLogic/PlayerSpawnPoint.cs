d80b53410db4b5a0f1a1e8117d83393af8c59bf8
KANG JIHYE <tessa.kangjihye@gmail.com>
2024-12-02 13:34:34 +0100

    Start Gate

--- Assets/Global/Scripts/RoomLogic/PlayerSpawnPoint.cs
+++ Assets/Global/Scripts/RoomLogic/PlayerSpawnPoint.cs
@@ -8,12 +8,14 @@ public class PlayerSpawnPoint : MonoBehaviour
     }
 
     public void SpawnPoint() {
+        Vector3 offset = new Vector3(0, 0, -2);
+
         var playerObj = GlobalReference.GetReference<PlayerReference>().PlayerObj;
-        playerObj.transform.position = this.transform.position;
+        playerObj.transform.position = this.transform.position + offset;
         playerObj.transform.rotation = this.transform.rotation;
 
         var SmoothCamaraTarget = GlobalReference.GetReference<PlayerReference>().SmoothCamaraTarget;
-        SmoothCamaraTarget.transform.position = this.transform.position;
+        SmoothCamaraTarget.transform.position = this.transform.position + offset;
         SmoothCamaraTarget.transform.rotation = this.transform.rotation;
 
     }
