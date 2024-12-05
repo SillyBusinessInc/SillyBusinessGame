e51198d2e6b849a6ad776b963102b94d6de10a5e
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2024-12-05 10:45:51 +0100

    test

---
+++ Assets/Global/Scripts/EnemySpawner/Wave.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewWave", menuName = "ScriptableObjects/Wave")]
+public class Wave : ScriptableObject
+{
+    public List<WavePart> waveParts; // List of enemy types and counts
+    public float interval = 1; // delay interfal for the enemies
+
+}
