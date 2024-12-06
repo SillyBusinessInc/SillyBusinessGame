ff430bbf504e98cdbf182f8de67a0825e1054dfe
fabio <95278302+fabiothomas@users.noreply.github.com>
2024-12-06 10:36:31 +0100

    fixed vulnerability and added documentation to save system

--- Assets/Global/Scripts/SaveLogic/Secure/SecureSaveSystem.cs
+++ Assets/Global/Scripts/SaveLogic/Secure/SecureSaveSystem.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using NUnit.Framework.Constraints;
-using Unity.VisualScripting;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
 using UnityEngine;
 
 public abstract class SecureSaveSystem
 {
+    /// <summary> Defines the name of the save file </summary>
     protected abstract string Prefix {get;}
+    /// <summary> The initialization method. Use the Add() methods in this method to initialize the content of this save file </summary>
     public abstract void Init();
 
     public SecureSaveSystem() {
@@ -16,75 +18,94 @@ public abstract class SecureSaveSystem
         LoadAll();
     }
 
-    //Logic
+    // Logic
     private string SavePath => Path.Combine(Application.persistentDataPath, $"{Prefix}.mold");
     private readonly Dictionary<string, ISecureSaveable> saveables = new() {};
-    private readonly Dictionary<string, Type> saveableTypes = new() {};
+
+    /// <summary> Defines if this save file has unsaved changes </summary>
     public bool IsDirty { get; private set; } = false;
 
+    /// <summary> Finds the value with the given id </summary>
     public T Get<T>(string id) {
-        if (SerializableData.AllowedType(typeof(T)) == AllowedTypeReturnOption.ALLOWED_OBJECT) {
+        // check if type is a seriallized object that requires special handling
+        if (SerializableData.AllowedType(typeof(T)) == AllowedTypeReturnOption.ALLOWED_STRINGIFY) {
             return JsonUtility.FromJson<T>(Get<string>(id));
         }
+        // check if the value exists in this save system
         if (!saveables.ContainsKey(id)) {
             Debug.LogError($"{id} can not be found in {GetType().Name}");
             return default;
         }
+        // return the requested value
         return (saveables[id] as SecureSaveable<T>).Value;
     }
 
+    /// <summary> Sets the value with the given id to a new value </summary>
     public void Set<T>(string id, T value) {
-        if (SerializableData.AllowedType(typeof(T)) == AllowedTypeReturnOption.ALLOWED_OBJECT) {
+        // check if type is a seriallized object that requires special handling
+        if (SerializableData.AllowedType(typeof(T)) == AllowedTypeReturnOption.ALLOWED_STRINGIFY) {
             Set(id, JsonUtility.ToJson(value));
             return;
         }
+        // check if the value exists in this save system
         if (!saveables.ContainsKey(id)) {
             Debug.LogError($"{id} can not be found in {GetType().Name}");
             return;
         }
+        // set value to requested value and make save system dirty
         saveables[id].Set(value);
         IsDirty = true;
     }
 
+    /// <summary> Add a new value to the save system. This should generally be done in SecureSaveSystem.Init() </summary>
     protected void Add<T>(string id, T defaultValue) {
+        // check if type is allowed
         Type type = typeof(T);
         AllowedTypeReturnOption result = SerializableData.AllowedType(type);
         if (result == AllowedTypeReturnOption.DISALLOWED) Debug.LogError($"cannot save [{id}] because [{type}] is not a securely saveable type");
-        else if (result == AllowedTypeReturnOption.ALLOWED_OBJECT) {
+        // check if type is a seriallized object that requires special handling
+        else if (result == AllowedTypeReturnOption.ALLOWED_STRINGIFY) {
             Add(id, JsonUtility.ToJson(defaultValue));
             return;
         }
-        
+        // add value to the save system
         saveables.Add(id, new SecureSaveable<T>($"{Prefix}_{id}", defaultValue));
-        saveableTypes.Add(id, typeof(T));
     }
 
+    /// <summary> Saves all data stored in the save system to a file </summary>
     public void SaveAll() {
-        BinaryFormatter formatter = new();
+        // open file to write to
         using (FileStream stream = new(SavePath, FileMode.Create)) {
+            // create serializable data object for storing
             SerializableData data = new();
-
+            // populate data object with values from the save system
             foreach (KeyValuePair<string, ISecureSaveable> saveable in saveables) {
-                data.AddAllFromSecureSavable(saveable.Value);
+                data.PullFromSecureSavable(saveable.Value);
             }
-
-            formatter.Serialize(stream, data);
-            stream.Close();
+            // convert data object to file data for save storage
+            BinaryWriter writer = new(stream);
+            string jsonText = JsonConvert.SerializeObject(data);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(jsonText);
+            // write data to file
+            writer.Write(Convert.ToBase64String(plainTextBytes));
+            writer.Flush();
         }
         IsDirty = false;
     }
 
+    /// <summary> Loads all data stored in a file to the save system </summary>
     public void LoadAll() {
         if (File.Exists(SavePath)) {
-            BinaryFormatter formatter = new();
+            // open file to read from
             using (FileStream stream = new(SavePath, FileMode.Open)) {
-                SerializableData data = (SerializableData)formatter.Deserialize(stream); // load into data element - then turn data element in saveables
-
+                // convert file data to data object for loading
+                BinaryReader reader = new(stream);
+                byte[] encodedBytes = Convert.FromBase64String(reader.ReadString());
+                SerializableData data = JsonConvert.DeserializeObject<SerializableData>(Encoding.UTF8.GetString(encodedBytes));
+                // populate current save system with values from data object
                 foreach (KeyValuePair<string, ISecureSaveable> saveable in saveables) {
-                    data.UpdateSecureSaveable(saveable.Value);
+                    data.PushToSecureSaveable(saveable.Value);
                 }
-                
-                stream.Close();
             }
             IsDirty = false;
         } 
@@ -93,7 +114,7 @@ public abstract class SecureSaveSystem
         }
     }
 
-    // debug
+    /// <summary> Debug Method: Lists all values of the given type </summary>
     public void ListAll<T>() {
         foreach (KeyValuePair<string, ISecureSaveable> saveable in saveables) {
             if (saveable.Value is SecureSaveable<T> t) Debug.Log($"{t.Id}: {t.Value}");
--- Assets/Global/Scripts/SaveLogic/Simple/SaveSystem.cs
+++ Assets/Global/Scripts/SaveLogic/Simple/SaveSystem.cs
@@ -4,45 +4,58 @@ using UnityEngine;
 
 public abstract class SaveSystem
 {
+    /// <summary> Defines the name of the save file </summary>
     protected abstract string Prefix {get;}
+    /// <summary> The initialization method. Use the Add() methods in this method to initialize the content of this save file </summary>
     public abstract void Init();
 
     public SaveSystem() {
         Init();
     }
 
-    //Logic
+    // Logic
     private readonly Dictionary<string, ISaveable> saveables = new() {};
+
+    /// <summary> Defines if this save file has unsaved changes </summary>
     public bool IsDirty { get; private set; } = false;
 
+    /// <summary> Finds the value with the given id </summary>
     public T Get<T>(string id) {
+        // check if the value exists in this save system
         if (!saveables.ContainsKey(id)) {
             Debug.LogError($"{id} can not be found in {GetType().Name}");
             return default;
         }
+        // return the requested value
         return (saveables[id] as Saveable<T>).Value;
     }
 
+    /// <summary> Sets the value with the given id to a new value </summary>
     public void Set<T>(string id, T value) {
+        // check if the value exists in this save system
         if (!saveables.ContainsKey(id)) {
             Debug.LogError($"{id} can not be found in {GetType().Name}");
             return;
         }
+        // set value to requested value and make save system dirty
         saveables[id].Set(value);
         IsDirty = true;
     }
 
+    /// <summary> Add a new value to the save system. This should generally be done in SaveSystem.Init() </summary>
     protected void Add<T>(string id, T defaultValue) {
+        // check if type is allowed
         Type type = typeof(T);
         if (type != typeof(int) &&
             type != typeof(float) &&
             type != typeof(string) &&
             type != typeof(bool)
         ) Debug.LogError($"cannot save {id} because {type} is not a saveable type. please only save int, float, string or bool");
-        
+        // add value to the save system
         saveables.Add(id, new Saveable<T>($"{Prefix}_{id}", defaultValue));
     }
 
+    /// <summary> Saves all data stored in the save system to PlayerPrefs </summary>
     public void SaveAll() {
         foreach (KeyValuePair<string, ISaveable> saveable in saveables) {
             saveable.Value.Save();
@@ -50,6 +63,7 @@ public abstract class SaveSystem
         IsDirty = false;
     }
 
+    /// <summary> Loads all data stored in PlayerPrefs to the save system </summary>
     public void LoadAll() {
         foreach (KeyValuePair<string, ISaveable> saveable in saveables) {
             saveable.Value.Load();
@@ -57,7 +71,7 @@ public abstract class SaveSystem
         IsDirty = false;
     }
 
-    // debug
+    /// <summary> Debug Method: Lists all values of the given type </summary>
     public void ListAll<T>() {
         foreach (KeyValuePair<string, ISaveable> saveable in saveables) {
             if (saveable.Value is Saveable<T> t) Debug.Log($"{t.Id}: {t.Value}");
