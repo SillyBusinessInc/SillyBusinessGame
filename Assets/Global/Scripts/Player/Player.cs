f3397a20702bf1c866719337a51d84251cf22899
Isis <89838626+Isissss@users.noreply.github.com>
2024-12-11 15:40:08 +0100

    feat: invulnarability

--- Assets/Global/Scripts/Player/Player.cs
+++ Assets/Global/Scripts/Player/Player.cs
@@ -30,6 +30,7 @@ public class Player : MonoBehaviour
     public float groundCheckAngle = 50.0f;
     public float maxIdleTime = 20f;
     public float minIdleTime = 5f;
+    [SerializeField] private float invulnerabilityTime = 0.5f;
 
 
     [Header("Stats")]
@@ -82,6 +83,7 @@ public class Player : MonoBehaviour
     private bool IsLanding = false;
     [SerializeField] private Image fadeImage;
     [SerializeField] private CrossfadeController crossfadeController;
+    private bool isInvulnerable = false;
 
 
     void Start()
@@ -121,7 +123,7 @@ public class Player : MonoBehaviour
         GlobalReference.UnsubscribeTo(Events.PLAYER_ATTACK_STARTED, attackingAnimation);
         GlobalReference.UnsubscribeTo(Events.PLAYER_ATTACK_ENDED, attackingStoppedAnimation);
     }
-    
+
     private void OnDrawGizmos()
     {
         Debug.DrawLine(rb.position, rb.position + targetVelocity, debug_lineColor, 0, true);
@@ -166,7 +168,7 @@ public class Player : MonoBehaviour
                     if (Vector3.Angle(Vector3.up, hit.normal) < groundCheckAngle)
                     {
                         currentJumps = 0;
-                        if(!isGrounded)
+                        if (!isGrounded)
                             Tail.attackIndex = 0;
                         isGrounded = true;
                         playerAnimationsHandler.SetBool("IsOnGround", true);
@@ -199,7 +201,7 @@ public class Player : MonoBehaviour
             }
         }
     }
-    
+
     public void SetState(StateBase newState)
     {
         // stop active coroutine
@@ -259,7 +261,7 @@ public class Player : MonoBehaviour
     {
         // return if there is no target velocity to move towards | currently disabled as I'm investigating it's necessity
         // if (targetVelocity == Vector3.zero) return;
-        
+
         // slowly move to target velocity
         Vector3 newVelocity = Vector3.MoveTowards(rb.linearVelocity, targetVelocity, currentMovementLerpSpeed * Time.deltaTime);
 
@@ -285,11 +287,22 @@ public class Player : MonoBehaviour
     // If we go the event route this should change right?
     public void OnHit(float damage)
     {
+        if (isInvulnerable) return;
         playerStatistic.Health -= damage;
         GlobalReference.AttemptInvoke(Events.HEALTH_CHANGED);
 
         if (playerStatistic.Health <= 0) OnDeath();
+
+        isInvulnerable = true;
+        StartCoroutine(InvulnerabilityTimer());
     }
+
+    private IEnumerator InvulnerabilityTimer()
+    {
+        yield return new WaitForSeconds(invulnerabilityTime);
+        isInvulnerable = false;
+    }
+
 
     public void applyKnockback(Vector3 knockback, float time)
     {
@@ -308,12 +321,14 @@ public class Player : MonoBehaviour
         GlobalReference.AttemptInvoke(Events.HEALTH_CHANGED);
     }
 
-    public void MultiplyMaxHealth(float reward) {
+    public void MultiplyMaxHealth(float reward)
+    {
         playerStatistic.MaxHealth.AddMultiplier("reward", reward, true);
         Heal(1f);
     }
-    
-    public void IncreaseMaxHealth(float reward) {
+
+    public void IncreaseMaxHealth(float reward)
+    {
         playerStatistic.MaxHealth.AddModifier("reward", reward);
         Heal(1f);
     }
@@ -324,7 +339,8 @@ public class Player : MonoBehaviour
     {
         StartCoroutine(DeathScreen());
     }
-    private IEnumerator DeathScreen() {
+    private IEnumerator DeathScreen()
+    {
         Debug.Log("Player died", this);
         yield return StartCoroutine(crossfadeController.Crossfade_Start());
         SceneManager.LoadScene("Death");
