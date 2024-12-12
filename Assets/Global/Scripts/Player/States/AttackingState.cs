e46c0e3963b04716368189e355d9fce2f835d5f2
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2024-12-12 16:56:31 +0100

    cant spam attack

--- Assets/Global/Scripts/Player/States/AttackingState.cs
+++ Assets/Global/Scripts/Player/States/AttackingState.cs
@@ -2,7 +2,7 @@ using UnityEngine.InputSystem;
 
 public class AttackingState : StateBase
 {
-    public AttackingState(Player player) : base(player) {}
+    public AttackingState(Player player) : base(player) { }
 
     public void IncreaseIndex()
     {
@@ -11,11 +11,11 @@ public class AttackingState : StateBase
                 ? 0
                 : ++Player.Tail.attackIndex;
     }
-    
+
     public override void Enter()
     {
         var tail = Player.Tail.currentTail;
-        if (Player.AirComboDone || !(Player.Tail.activeCooldownTime >= Player.Tail.cooldownTime) || tail.currentCombo.Count == 0 || Player.isAttacking)
+        if (Player.AirComboDone || !(Player.Tail.activeCooldownTime >= Player.Tail.cooldownTime) || tail.currentCombo.Count == 0)
         {
             Player.SetState(Player.states.Falling);
             return;
@@ -23,7 +23,7 @@ public class AttackingState : StateBase
         Player.targetVelocity *= 0;
         Player.rb.linearVelocity *= 0;
         Player.Tail.activeCooldownTime = 0.0f;
-        Player.Tail.currentTail.currentCombo = Player.isGrounded? Player.Tail.currentTail.groundCombo : Player.Tail.currentTail.airCombo;
+        Player.Tail.currentTail.currentCombo = Player.isGrounded ? Player.Tail.currentTail.groundCombo : Player.Tail.currentTail.airCombo;
         Player.AirComboDone = !Player.isGrounded && Player.Tail.attackIndex >= Player.Tail.currentTail.airCombo.Count - 1;
         var currentCombo = tail.currentCombo[Player.Tail.attackIndex];
         currentCombo.Start();
@@ -33,11 +33,25 @@ public class AttackingState : StateBase
 
     public override void Exit()
     {
+        Player.targetVelocity *= 0;
         Player.playerAnimationsHandler.animator.speed = 1.0f;
         Player.Tail.flipCanDoDamage = false;
         Player.Tail.tailCanDoDamage = false;
         Player.collidersEnemy.Clear();
     }
 
-    public override void Attack(InputAction.CallbackContext ctx) {}
+    public override void Attack(InputAction.CallbackContext ctx) { }
+
+    public override void Move(InputAction.CallbackContext ctx) { }
+
+    public override void Sprint(InputAction.CallbackContext ctx) { }
+
+    public override void Dodge(InputAction.CallbackContext ctx) { }
+
+    public override void Jump(InputAction.CallbackContext ctx) { }
+
+    public override void Glide(InputAction.CallbackContext ctx) { }
+
+    public override void Crouch(InputAction.CallbackContext ctx) { }
+
 }
