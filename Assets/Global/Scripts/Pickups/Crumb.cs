aedc582e8262a532b3611594bfe1d4e2d02fff34
SuaveGiant <carlorubio@live.nl>
2024-12-16 15:42:52 +0100

    Crumbs and sfx

--- Assets/Global/Scripts/Pickups/Crumb.cs
+++ Assets/Global/Scripts/Pickups/Crumb.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
 
-public class Crumb: MonoBehaviour
+public class Crumb : MonoBehaviour
 {
-    //
+    public AudioClip pickupSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    private bool isCollected = false;
+
     public void OnTriggerEnter(Collider other)
     {
+        if (isCollected) return;
+
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            isCollected = true;
+
+            if (pickupSound != null)
+            {
+                audioSource.PlayOneShot(pickupSound);
+            }
+
+            Destroy(gameObject, pickupSound.length);
         }
     }
+
 }
