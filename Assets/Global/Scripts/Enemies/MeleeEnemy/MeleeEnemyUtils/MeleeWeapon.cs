6e8f41822dbf4be35fd76f33c7fff7375b5f2b74
Pim van Milt <pim.milt@gmail.com>
2024-12-19 11:53:01 +0100

    weapon cleanup

--- Assets/Global/Scripts/Enemies/MeleeEnemy/MeleeEnemyUtils/MeleeWeapon.cs
+++ Assets/Global/Scripts/Enemies/MeleeEnemy/MeleeEnemyUtils/MeleeWeapon.cs
@@ -2,18 +2,6 @@ using UnityEngine;
 
 namespace EnemiesNS
 {
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //TODO: SET COLLIDER SIZE BACK TO NORMAL AND DELETE WHEN ATTACK ANIMATIONS HAVE BEEN IMPLEMENTED               //
-    //                                                                                                             //
-    // ENLARGED THE COLLIDER FOR MELEE DUE TO MISSING ATTACK ANIMS                                                 //
-    // ORIGINAL CENTER: Vector3 Vector3(0,-0.0656406805,1.38706756)                                                                                                         //
-    // ORIGINAL SIZE: Vector3(0.193226293,0.370392919,2.06135321)                                                     //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
     public class MeleeWeapon : MonoBehaviour
     {
         EnemyBase enemy;
